48521c7a5f2c042bec189fab73dfc9a6fa58cfd3
Fabian Morón Zirfas <hash@ff6347.email>
2025-10-30 10:41:52 +0100

    chore: Remove logging wrap in try catch

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const tocLinkClickHandler = (event) => {
 	event.preventDefault();
-	console.log(event.target);
+
 	const url = new URL(event.target.href);
-	console.log(url);
+
 	const target = document.querySelector(url.hash);
 	target.scrollIntoView({ behavior: "smooth", block: "start" });
 };
@@ -20,70 +20,73 @@ window.addEventListener("load", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-	const title = document.querySelector(".title");
-	if (!title) {
-		console.error("could not find title");
-		return;
-	}
-	title.scrollIntoView({ behavior: "smooth", block: "start" });
+	try {
+		const title = document.querySelector(".title");
+		if (!title) {
+			console.error("could not find title");
+			return;
+		}
+		title.scrollIntoView({ behavior: "smooth", block: "start" });
 
-	const tocs = document.querySelectorAll("toc-item");
+		const tocs = document.querySelectorAll("toc-item");
 
-	tocs.forEach((item) => {
-		item.addEventListener("click", tocLinkClickHandler);
-	});
+		tocs.forEach((item) => {
+			item.addEventListener("click", tocLinkClickHandler);
+		});
 
-	const tocLinks = document.querySelectorAll(".toc-link");
-	const targetContainers = document.querySelectorAll("section.card");
+		const tocLinks = document.querySelectorAll(".toc-link");
+		const targetContainers = document.querySelectorAll("section.card");
 
-	// For each target container
-	targetContainers.forEach((container) => {
-		if (!container.classList.contains("navigation")) {
-			// create one link that points back to the nav
-			const parent = document.createElement("div");
-			parent.classList.add("backlinks");
-			const link = document.createElement("a");
-			link.innerHTML = "Abyssinian Hare";
-			link.href = "#navigation";
-			link.addEventListener("click", tocLinkClickHandler);
-			const tocItem = document.createElement("toc-item");
-			const tocCircle = document.createElement("circle-item");
-			tocItem.appendChild(tocCircle);
-			tocItem.appendChild(link);
-			parent.appendChild(tocItem);
-
-			// now clone all the others
-			tocLinks.forEach((link) => {
-				const clone = link.cloneNode(true);
-				clone.addEventListener("click", tocLinkClickHandler);
+		// For each target container
+		targetContainers.forEach((container) => {
+			if (!container.classList.contains("navigation")) {
+				// create one link that points back to the nav
+				const parent = document.createElement("div");
+				parent.classList.add("backlinks");
+				const link = document.createElement("a");
+				link.innerHTML = "Abyssinian Hare";
+				link.href = "#navigation";
+				link.addEventListener("click", tocLinkClickHandler);
 				const tocItem = document.createElement("toc-item");
 				const tocCircle = document.createElement("circle-item");
 				tocItem.appendChild(tocCircle);
-				tocItem.appendChild(clone);
+				tocItem.appendChild(link);
 				parent.appendChild(tocItem);
-				container.appendChild(parent);
-			});
-		}
 
-		// Clone all TOC links and append to this container
-	});
+				// now clone all the others
+				tocLinks.forEach((link) => {
+					const clone = link.cloneNode(true);
+					clone.addEventListener("click", tocLinkClickHandler);
+					const tocItem = document.createElement("toc-item");
+					const tocCircle = document.createElement("circle-item");
+					tocItem.appendChild(tocCircle);
+					tocItem.appendChild(clone);
+					parent.appendChild(tocItem);
+					container.appendChild(parent);
+				});
+			}
 
-	// cleanup
+			// Clone all TOC links and append to this container
+		});
 
-	document.querySelectorAll(".cite-bracket,.reference").forEach((item) => {
-		item.remove();
-	});
+		// cleanup
 
-	document.querySelectorAll("a").forEach((anchor) => {
-		if (
-			anchor.href?.includes("/wiki/") &&
-			!anchor.classList.contains("toc-link")
-		) {
-			console.log("has wiki ", anchor.href);
-			// external link lets make them point at the wikipedia again
-			const url = new URL(anchor.href);
-			anchor.target = "_blank";
-			anchor.href = `https://en.wikipedia.org${url.pathname}`;
-		}
-	});
+		document.querySelectorAll(".cite-bracket,.reference").forEach((item) => {
+			item.remove();
+		});
+
+		document.querySelectorAll("a").forEach((anchor) => {
+			if (
+				anchor.href?.includes("/wiki/") &&
+				!anchor.classList.contains("toc-link")
+			) {
+				// external link lets make them point at the wikipedia again
+				const url = new URL(anchor.href);
+				anchor.target = "_blank";
+				anchor.href = `https://en.wikipedia.org${url.pathname}`;
+			}
+		});
+	} catch (e) {
+		console.error(e);
+	}
 });
